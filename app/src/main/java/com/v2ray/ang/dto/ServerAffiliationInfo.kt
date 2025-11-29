12f715c3d51b5262690826a420ec7b8929c97295
xiexievpn <zhaifangyuan2@gmail.com>
2025-11-29 23:09:57 +0800

    Clean Android 客户端 source code sync - 2025-11-29

---
+++ app/src/main/java/com/v2ray/ang/dto/ServerAffiliationInfo.kt
@@ -0,0 +1,1 @@
+package com.v2ray.ang.dtodata class ServerAffiliationInfo(var testDelayMillis: Long = 0L) {    fun getTestDelayString(): String {        if (testDelayMillis == 0L) {            return ""        }        return testDelayMillis.toString() + "ms"    }}
---
+++ build.gradle.kts
@@ -0,0 +1,1 @@
+plugins {    alias(libs.plugins.android.application) apply false    alias(libs.plugins.android.library) apply false    alias(libs.plugins.kotlin.android) apply false}buildscript {    dependencies {        classpath(libs.gradle.license.plugin)    }}
