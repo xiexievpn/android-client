7eae5abed6ae43e2d7b49e9f894bc158674407e5
xiexievpn <zhaifangyuan2@gmail.com>
2026-01-29 09:43:21 +0800

    Clean Android 客户端 source code sync - 2026-01-29

---
+++ V2rayNG/app/src/main/java/com/v2ray/ang/plugin/NativePlugin.kt
@@ -0,0 +1,1 @@
+package com.v2ray.ang.pluginimport android.content.pm.ResolveInfoclass NativePlugin(resolveInfo: ResolveInfo) : ResolvedPlugin(resolveInfo) {    init {        check(resolveInfo.providerInfo != null)    }    override val componentInfo get() = resolveInfo.providerInfo!!}
